b8bc7a1023b6c02f381c7782fa7fb55bf90d5df7
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-09 20:13:36 +0200

    Create scp.js

---
+++ funni bitburner things/scp.js
@@ -0,0 +1,7 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	const args = ns.flags([["help", false]])
+	const files = ns.ls(ns.getHostname(), ".lit")
+	const dest = args._[0]
+	await ns.scp(files, dest)
+}
