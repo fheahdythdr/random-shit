77316aa9b943c7a2ae0ba1f022f5ef1a059405f7
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-09 21:31:34 +0200

    Update getFiles.js

--- funni bitburner things/getFiles.js
+++ funni bitburner things/getFiles.js
@@ -3,12 +3,13 @@ export async function main(ns) {
 	const args = ns.flags([["help", false]]);
 	const host = args._[0];
     	if (!ns.hasRootAccess(host)) {
-			if (!ns.fileExists("remoteHack.js", ns.)) {
-				await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/remoteHack.js", "remoteHack.js", "home")
+			if (!ns.fileExists("remoteHack.js", ns.getServer())) {
+				await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/remoteHack.js", "remoteHack.js", "home");
 			}
-			ns.run("remoteHack.js", 1, host)
+			ns.toast("No root access, running remote hack..", "info");
+			ns.run("remoteHack.js", 1, host);
 	}
-	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/modifiedscp.js", "scp.js", host)
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/modifiedscp.js", "scp.js", host);
 	ns.exec("scp.js",host);
 	ns.rm("scp.js", host)
 }
