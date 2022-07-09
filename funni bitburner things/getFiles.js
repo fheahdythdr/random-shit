72fba80cdcbedbdf0ccc7ff41003dd1ca3c67a6b
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-09 21:04:43 +0200

    Update getFiles.js

--- funni bitburner things/getFiles.js
+++ funni bitburner things/getFiles.js
@@ -2,7 +2,9 @@
 export async function main(ns) {
 	const args = ns.flags([["help", false]]);
 	const host = args._[0];
-    ns.run("remoteHack.js", 1, host)
+    	if (!ns.hasRootAccess(host)) {
+		ns.run("remoteHack.js", 1, host)
+	}
 	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/modifiedscp.js", "scp.js", host)
 	ns.exec("scp.js",host);
 	ns.rm("scp.js", host)
