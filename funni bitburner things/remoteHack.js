78a4d6f6b5e9837c7d34caa8663f12c612acdd3f
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-09 08:04:29 +0200

    Update remoteHack.js

--- funni bitburner things/remoteHack.js
+++ funni bitburner things/remoteHack.js
@@ -1,13 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	const args = ns.flags([["help", false]]);
-	if (args.help || args._.length < 2) {
-		ns.tprint("this script do the funni and hack server, giving you root access");
-		ns.tprint(`Usage: run ${ns.getScriptName()} SERVER`);
-		ns.tprint("Example:");
-		ns.tprint(`> run ${ns.getScriptName()} n00dles`);
-		return;
-	}
 	const host = args._[0];
 	await ns.brutessh(host)
 	await ns.sqlinject(host)
