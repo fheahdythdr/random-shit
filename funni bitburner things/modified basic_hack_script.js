18f13370135427608960e8b8be987d1bf6c73e1b
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-10 04:53:47 +0200

    Update modified basic_hack_script.js

--- funni bitburner things/modified basic_hack_script.js
+++ funni bitburner things/modified basic_hack_script.js
@@ -2,6 +2,7 @@
 export async function main(ns) {
     const args = ns.flags([['help', false]]);
     const hostname = args._[0];
+    ns.share()
     while (true) {
         if (ns.getServerSecurityLevel(hostname) > ns.getServerMinSecurityLevel(hostname) + 2.5) {
             await ns.weaken(hostname);
