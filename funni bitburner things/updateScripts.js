f762c649913dabe250ff78fc9c2b92dc60cf8c1f
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-10 05:12:08 +0200

    Update updateScripts.js

--- funni bitburner things/updateScripts.js
+++ funni bitburner things/updateScripts.js
@@ -4,4 +4,5 @@ export async function main(ns) {
 	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/modified%20basic_hack_script.js", "hackScript.js");
 	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/remoteHack.js", "remoteHack.js");
 	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/runHack.js", "runHack.js");
+	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/updateScripts.js", "updateScripts.js")
 }
