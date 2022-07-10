999f444947b520ec51684b55d5a1279dacf84060
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-07-10 17:40:08 +0200

    Update updateScripts.js

--- funni bitburner things/updateScripts.js
+++ funni bitburner things/updateScripts.js
@@ -1,9 +1,9 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/buyServer.js", "buyServer.js");
-	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/getFiles.js", "getFiles.js");
-	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/modified%20basic_hack_script.js", "hackScript.js");
-	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/remoteHack.js", "remoteHack.js");
-	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/runHack.js", "runHack.js");
-	await wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/updateScripts.js", "updateScripts.js")
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/buyServer.js", "buyServer.js");
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/getFiles.js", "getFiles.js");
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/modified%20basic_hack_script.js", "hackScript.js");
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/remoteHack.js", "remoteHack.js");
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/runHack.js", "runHack.js");
+	await ns.wget("https://raw.githubusercontent.com/fheahdythdr/random-shit/main/funni%20bitburner%20things/updateScripts.js", "updateScripts.js")
 }
