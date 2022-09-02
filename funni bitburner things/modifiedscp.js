f5de6e6cbe0d424f160cf7bb5778a178f4369b1e
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2022-09-02 16:30:36 +0200

    Update modifiedscp.js

--- funni bitburner things/modifiedscp.js
+++ funni bitburner things/modifiedscp.js
@@ -1,6 +1,8 @@
 /** @param {NS} ns */
 export async function main(ns) {
+	const args = ns.flags([["help", false]]);
+	const source = args._[0];
 	const files = ns.ls(ns.getHostname(), ".lit");
 	const dest = "home";
-	await ns.scp(files, dest);
+	await ns.scp(files, dest, source);
 }
